f97f12bbe2da51bb5f93ee0d8f715e52f5a65bea
jovemTranquilao <jovem@hocem.com>
2023-08-20 21:44:23 -0300

    salvando dados e recuperando find all falta colocar na tela e fazer o associa chefe

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -1,5 +1,56 @@
+import 'dart:html';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+Future<Colaborador> post(String nome, String senha, _MyHomePageState _myHomePageState) async {
+  final response = await http.post(
+    Uri.parse('http://localhost:8084'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'nome': nome,
+      'senha': senha,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    _myHomePageState.limparCampos();
+
+    return Colaborador.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to create colaborador.');
+  }
+}
+
+Future<List<Colaborador>> get() async {
+
+  final response = await http
+      .get(Uri.parse('http://localhost:8082'));
+
+  if (response.statusCode == 200) {
+    return List<Colaborador>.from( json.decode(response.body) .map( (x) => Colaborador.fromJson(x)));
+
+  } else {
+    throw Exception('Failed to load colaborador');
+  }
+}
+
+class Colaborador {
+  final String nome;
+  final String senha;
+
+  const Colaborador({required this.nome, required this.senha});
+
+  factory Colaborador.fromJson(Map<String, dynamic> json) {
+    return Colaborador(
+      nome: json['nome'],
+      senha: json['senha'],
+    );
+  }
+}
 void main() {
   runApp(const MyApp());
 }
@@ -7,46 +58,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Frontend flutter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Cadastro de colaboradores'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,54 +83,49 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String? _nome;
+  String? _senha;
+  List<Colaborador>? colaboradorList;
+
+  final nomeController = TextEditingController();
+  final senhaController = TextEditingController();
+
+  @override
+  void dispose() {
+    nomeController.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  void _salvar() {
+    _nome = nomeController.text;
+    _senha = senhaController.text;
+
+    post(_nome!, _senha!, this);
+    _counter = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
+    carregaLista();
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Align(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
@@ -112,6 +134,28 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            TextField(
+               decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Nome',
+              ),
+              controller: nomeController,
+            ),
+            TextField(
+               obscureText: true,
+               decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Senha',  
+              ),
+              controller: senhaController,
+            ),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: _salvar,
+              child: Text('Salvar'),
+            ),
           ],
         ),
       ),
@@ -119,7 +163,25 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
+  
+  void limparCampos() {
+    nomeController.text = '';
+    senhaController.text = '';
+    carregaLista();
+  }
+
+  void carregaLista(){
+    get().then(
+      (value) => lista(value)
+    );
+  }
+
+  void lista(List<Colaborador> lista){
+    colaboradorList = lista;
+  }
 }
+
+
